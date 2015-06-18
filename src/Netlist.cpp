cf74a5745f920dd0a870b802c8cb501f40ea9dd4
Diego Cruz <diego.cpcruz@gmail.com>
2015-06-18 12:56:02 -0300

    Adicionado método 'addCurrentVariable()' na classe 'Netlist'

--- src/Netlist.cpp
+++ src/Netlist.cpp
@@ -1,3 +1,4 @@
+#include "Element.h"
 #include "Netlist.h"
 
 Netlist::Netlist()
@@ -8,4 +9,34 @@ Netlist::Netlist()
 Netlist::~Netlist()
 {
     //dtor
+}
+
+void Netlist::addCurrentVariable(Element& element)
+{
+    string type = element.getType();
+    string name = element.getName();
+
+    if (type == 'V' || type == 'E' || type == 'F' || type == 'O')
+    {
+        m_NumVariables++;
+
+        if (m_NumVariables > MAX_NODES)
+        {
+            cout << "As correntes extras excederam o número de variáveis permitido ("
+                 << MAX_VARIABLES << ")" << endl;
+
+            return;
+        }
+
+        element.m_X = m_NumVariables;
+        m_VariablesList[m_NumVariables] = "j" + name;
+    }
+    else if (type == 'H')
+    {
+        m_NumVariables += 2;
+        element.m_X = m_NumVariables - 1;
+        element.m_Y = m_NumVariables;
+        m_VariablesList[m_NumVariables - 1] = "jx" + name;
+        m_VariablesList[m_NumVariables] = "jy" + name;
+    }
 }
