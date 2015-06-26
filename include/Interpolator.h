af054195f499757d373d5a6cd66b24bcddf92103
alexandre-slp <alexandre_slp@poli.ufrj.br>
2015-06-25 22:20:52 -0300

    Primeiro commit teste com as modificaçoes iniciais no interpolator

--- include/Interpolator.h
+++ include/Interpolator.h
@@ -3,16 +3,27 @@
 
 #include <vector>
 #include <complex>
+#include <string>
 
 using namespace std;
 
 class Interpolator
 {
     public:
-        Interpolator();
-        vector< complex<double> > getSValues(string mode, double radius); // Usar std::polar
-    protected:
+        Interpolator(string mode, double radius, int order) {
+        	m_Mode = mode;
+        	m_Radius = radius;
+        	m_Order = order;
+        }
+        ~Interpolator();
+
+        vector< complex<double> > getSValues(string mode, double radius, int order); // Usar std::polar
+
     private:
+        string m_Mode;
+        int m_Order;
+        double m_Radius;
+
 };
 
 #endif // INTERPOLATOR_H
--- src/Interpolator.cpp
+++ src/Interpolator.cpp
@@ -1,6 +1,32 @@
+#include <vector>
+#include <complex>
 #include "Interpolator.h"
 
-Interpolator::Interpolator()
-{
-    //ctor
-}
+using namespace std;
+
+ vector< complex<double> > Interpolator::getSValues(string mode, double radius, int order)
+ 	 {
+	 	 double step, beggin, point = 0;
+	 	 vector< complex<double> > values;
+
+	 	 step = 2*m_Radius/m_Order;
+
+	 	 if (mode == "LIN")
+	 	 {
+	 		 point = beggin = -radius;
+	 	 }
+
+	 	 if (mode == "CIR")
+	 	 {
+
+	 	 }
+
+	 	 for (int i = 0; i < N; i++)
+	 	 {
+	 		 complex<double> currValue;
+
+	 		 values.push_back(currValue);
+	 	 }
+
+	 	 return values;
+     }
