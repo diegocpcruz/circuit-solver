c5ad77ac75289c97f04c748b62585448d5f64c3a
Alexandre Paes <alexandre_slp@poli.ufrj.br>
2015-06-26 16:02:41 -0300

    Logica terminada, falta verificar se esta funcionando.

--- src/Interpolator.cpp
+++ src/Interpolator.cpp
@@ -4,45 +4,44 @@
 
 using namespace std;
 
- vector< complex<double> > Interpolator::getSValues(string mode, double radius, int order)
+ vector< complex<double> > Interpolator::getSValues(string mode, double radius)
  	 {
-	 	 double step, beggin = 0;
-	 	 complex<double> point = 0;
+	 	 double step = 0;
+	 	 complex<double> sValue;
 	 	 vector< complex<double> > values;
-
-	 	 step = 2*m_Radius/m_Order;
+//	 	 Linear:
+	 	 double real = radius;
+	 	 double imag = 0;
+//	 	 Circular:
+	 	 double modulus = radius;
+	 	 double phase = 0;
 
 	 	 if (mode == "LIN")
 	 	 {
-	 		 point = beggin = -radius;
-	 		 while (point <= radius)
+	 		 step = 2*m_Radius/m_Order;
+	 		 sValue = (-real , imag);
+	 		 while (real(sValue) <= radius)
 	 		 {
-	 			 if (point == 0)
+	 			 if (real(sValue) == 0)
 	 			 {
-	 				 point = -step/2;
-	 				 values.push_back(point);
-					 point += step;
-					 values.push_back(point);
-					 point += step/2;
+	 				 sValue += (step/2 , imag);
+	 				 values.push_back(sValue);
+	 				 sValue += (step/2 , imag);
 	 			 }
-	 			 else
-	 			 {
-	 				values.push_back(point);
-					 point += step;
-	 			 }
+				 values.push_back(sValue);
+				 sValue += (step , imag);
 	 		 }
 	 	 }
 
 	 	 if (mode == "CIR")
 	 	 {
-
-	 	 }
-
-	 	 for (int i = 0; i < N; i++)
-	 	 {
-	 		 complex<double> currValue;
-
-	 		 values.push_back(currValue);
+	 		 step = 360/m_Order;
+	 		 sValue = polar(modulus , phase);
+	 		 while (arg(sValue) <= 360)
+	 		 {
+	 			 values.push_back(sValue);
+	 			 sValue += polar(modulus , step);
+	 		 }
 	 	 }
 
 	 	 return values;
